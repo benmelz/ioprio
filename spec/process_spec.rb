d3e4dd7ffeb4f192ed4c6cae69bda6f797dff0d6
benmelz <ben@melz.me>
2025-04-02 15:08:42 -0400

    write specs and stubs for masking methods

--- spec/process_spec.rb
+++ spec/process_spec.rb
@@ -33,6 +33,47 @@ RSpec.describe Process do
     expect(described_class::IOPRIO_WHO_USER).to eq(3)
   end
 
+  describe ".ioprio_prio_class" do
+    { "none" => 0, "realtime" => 1, "best effort" => 2, "idle" => 3 }.each do |class_name, value|
+      it "decodes at the lower bound of the #{class_name} class" do
+        priority = value << 13
+        expect(described_class.ioprio_prio_class(priority)).to eq(value)
+      end
+
+      it "decodes at the upper bound of the #{class_name} class" do
+        priority = (value << 13) + 2.pow(13) - 1
+        expect(described_class.ioprio_prio_class(priority)).to eq(value)
+      end
+    end
+  end
+
+  describe ".ioprio_prio_data" do
+    { "none" => 0, "realtime" => 1, "best effort" => 2, "idle" => 3 }.each do |class_name, value|
+      it "decodes at the lower bound of the #{class_name} class" do
+        priority = value << 13
+        expect(described_class.ioprio_prio_data(priority)).to eq(0)
+      end
+
+      it "decodes at the upper bound of the #{class_name} class" do
+        priority = (value << 13) + 2.pow(13) - 1
+        expect(described_class.ioprio_prio_data(priority)).to eq(2.pow(13) - 1)
+      end
+    end
+  end
+
+  describe ".ioprio_prio_value" do
+    { "none" => 0, "realtime" => 1, "best effort" => 2, "idle" => 3 }.each do |class_name, value|
+      it "encodes at the lower bound of the #{class_name} class" do
+        expect(described_class.ioprio_prio_value(value, 0)).to eq(value << 13)
+      end
+
+      it "encodes at the upper bound of the #{class_name} class" do
+        priority = (value << 13) + 2.pow(13) - 1
+        expect(described_class.ioprio_prio_value(value, 2.pow(13) - 1)).to eq((value << 13) + 2.pow(13) - 1)
+      end
+    end
+  end
+
   describe ".ioprio_get" do
     it "returns the correct io priority" do
       priority = loop_script(prefix: "ionice --class 3") do |pid|
