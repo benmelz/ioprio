9a6b98fea28b21c4e61583f3cad63a7870d6ac47
benmelz <ben@melz.me>
2025-04-02 14:19:38 -0400

    fix: add constant stubs for better ide completions (#6)

--- lib/ioprio/core_ext/process.rb
+++ lib/ioprio/core_ext/process.rb
@@ -3,6 +3,13 @@
 module Ioprio
   module CoreExt
     module Process
+      # actual values are initialized in C extension
+      # rubocop:disable Lint/LiteralAsCondition
+      IOPRIO_WHO_PROCESS = _ if false
+      IOPRIO_WHO_PGRP = _ if false
+      IOPRIO_WHO_USER = _ if false
+      # rubocop:enable Lint/LiteralAsCondition
+
       def self.included(klass)
         klass.extend ClassMethods
       end
