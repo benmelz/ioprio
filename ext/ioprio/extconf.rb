1ea82e571f01006583d3d66838cb8f8defad2feb
benmelz <ben@melz.me>
2026-09-05 10:20:51 -0400

    drop frozen string literal comments

--- ext/ioprio/extconf.rb
+++ ext/ioprio/extconf.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require "mkmf"
 
 # Makes all symbols private by default to avoid unintended conflict
--- lib/ioprio.rb
+++ lib/ioprio.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative "ioprio/core_ext"
 require_relative "ioprio/version"
 require_relative "ioprio/ioprio"
